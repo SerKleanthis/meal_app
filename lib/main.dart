208c208d2116b169ac4db990422bd657a49682ed
Seretis Kleanthis <serkleanthis@gmail.com>
2021-10-05 16:18:35 +0300

    Packages and new meal screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/categories_screen.dart';
-import 'package:meal_app/screens/category_meals_screen.dart';
+import 'package:meal_app/screens/screen_categories.dart';
+import 'package:meal_app/screens/screen_meals.dart';
 
 void main() => runApp(MyApp());
 
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/category_meals_screen.dart';
+import 'package:meal_app/screens/screen_meals.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
